5803caca98169f1e8f45f38ca58a4485cdbc853c
Ondřej Švanda <46406259+Papooch@users.noreply.github.com>
2025-11-29 23:53:21 +0100

    add Nuxt

---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Tak my se teda vemem, no</h1>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Tak my se teda vemem, no',
+  description: 'Oficiální web svatebního obřadu',
+})
+
+</script>
