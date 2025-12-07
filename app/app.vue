7adca854d94b8533d1b3910eb6f45de033acd2e3
Ondřej Švanda <46406259+Papooch@users.noreply.github.com>
2025-12-07 15:02:06 +0100

    add eslint and font

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <h1>Tak my se teda vemem, no</h1>
+    <h1>Tak my se teda vemem, no...</h1>
   </div>
 </template>
 
-
 <script setup lang="ts">
 useSeoMeta({
-  title: 'Tak my se teda vemem, no',
-  description: 'Oficiální web svatebního obřadu',
-})
-
+  title: "Tak my se teda vemem, no",
+  description: "Oficiální web svatebního obřadu",
+});
 </script>
+
+<style scoped>
+h1 {
+  font-family: "LemonTuesday", sans-serif;
+  font-weight: normal;
+  /* Disable typographic ligatures for this heading */
+  font-variant-ligatures: none;
+  font-feature-settings: "liga" 0, "clig" 0, "calt" 0;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@nuxt/eslint"],
   ssr: true,
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  compatibilityDate: "2025-07-15",
+  eslint: {
+    config: {
+      stylistic: {
+        indent: 2,
+        semi: true,
+        quotes: "double",
+      },
+    },
+  },
+});
